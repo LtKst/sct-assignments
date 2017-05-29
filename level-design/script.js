382202a202f360141ad8dd8c89428309c59b68a0
Koen Sparreboom <koensparreboom@gmail.com>
2017-05-29 11:48:23 +0200

    Update to Level Design

--- level-design/script.js
+++ level-design/script.js
@@ -1,31 +1,51 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 
-var image = new Image();
-image.src = "tankSheet.png";
-
 // fill the sprites array
 var sprites = [];
 var n = 32;
 var nRow = 8;
 var blockSize = 64;
 
-for (let i = 0; i < n; i++) {
-    var x = i % nRow * blockSize;
-    var y = Math.floor(i / nRow) * blockSize;
-
-    sprites[i] = new Sprite(image, x, y, 64, 64);
-}
-
 // fill the level
 var levelN = 64;
 var levelRow = 8;
 var tileSize = 64;
 
-for (let i = 0; i < levelN; i++)
-{
-    var x = i % levelRow * tileSize;
-    var y = Math.floor(i / levelRow) * tileSize;
-    
-    sprites[0].draw(x, y);
+var myLevel = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+];
+
+var image = new Image();
+image.src = "tank_sheet.png";
+
+image.addEventListener('load', function () {
+    setInterval(draw, 100);
+});
+
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // fill the sprites array
+    for (let i = 0; i < n; i++) {
+        var x = i % nRow * blockSize;
+        var y = Math.floor(i / nRow) * blockSize;
+
+        sprites[i] = new Sprite(image, x, y, blockSize, blockSize);
+    }
+
+    // fill the level
+    for (let i = 0; i < myLevel.length; i++) {
+        var x = i % levelRow * tileSize;
+        var y = Math.floor(i / levelRow) * tileSize;
+
+        sprites[myLevel[i]].draw(x, y);
+    }
 }
