51e29938148e57c43ed55ad9102e8b9f19edcae4
Koen Sparreboom <koensparreboom@gmail.com>
2017-05-29 12:17:28 +0200

    Added level randomizing

--- level-design/script.js
+++ level-design/script.js
@@ -19,7 +19,7 @@ var myLevel = [
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0
 ];
 
@@ -27,12 +27,14 @@ var image = new Image();
 image.src = "tank_sheet.png";
 
 image.addEventListener('load', function () {
-    setInterval(draw, 100);
+    randomizeLevel(15);
+    
+    setInterval(draw, 60);
 });
 
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-
+    
     // fill the sprites array
     for (let i = 0; i < n; i++) {
         var x = i % nRow * blockSize;
@@ -45,7 +47,28 @@ function draw() {
     for (let i = 0; i < myLevel.length; i++) {
         var x = i % levelRow * tileSize;
         var y = Math.floor(i / levelRow) * tileSize;
-
+        
         sprites[myLevel[i]].draw(x, y);
     }
+    
+    sprites[1].draw(0, 0);
+}
+
+function randomizeLevel(obstaclePercantage)
+{
+    for (let i = 0; i < myLevel.length; i++)
+    {
+        if (randomRange(0, 100) < obstaclePercantage)
+        {
+            myLevel[i] = randomRange(24, 30);
+        }
+        else
+        {
+            myLevel[i] = 0;
+        }
+    }
+}
+
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
