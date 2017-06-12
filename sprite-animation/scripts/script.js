74dd070a3575a085d8bed4a72c9a374b87315e31
Koen Sparreboom <koensparreboom@gmail.com>
2017-06-12 12:09:24 +0200

    Added new sprite & background

--- sprite-animation/scripts/script.js
+++ sprite-animation/scripts/script.js
@@ -4,15 +4,19 @@ var context = canvas.getContext('2d');
 var sx,sy,sw,sh,x,y,w,h;
 
 var image = new Image();
-image.src = "spr.png";
+image.src = "sprite-sheet.png";
 
-var i = 0;
+var wall = new Image();
+wall.src = "wall.png";
+var wallX = 0;
+
+var i = 9;
 var nRow = 4;
 
 image.addEventListener('load', ()=>
 {
     sw = image.width/4;
-    sh = image.height/2;
+    sh = image.height/4;
     
     w = sw;
     h = sh;
@@ -20,22 +24,40 @@ image.addEventListener('load', ()=>
     sx = 0;
     sy = 0;
     
-    x = canvas.width/2-sw/2;
-    y = canvas.height/2-sh/2;
+    x = 100;
+    y = canvas.height/2+15;
     
-    setInterval(animate, 100);
+    setInterval(animate, 175);
 });
 
 function animate()
 {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    context.drawImage(wall, wallX, 0);
+    
+    wallX -= 25;
+    
+    if (wallX <= 0 - canvas.width)
+    {
+        wallX = 0;
+    }
+    
     sx = (i%nRow)*sw;
     sy = Math.floor(i/nRow)*sh;
     
     context.drawImage(image, sx, sy, sw, sh, x, y, w, h);
     i++
     
-    if (i > 7)
+    /*x += 50;
+    
+    if (x >= canvas.width + sw/2)
     {
-        i = 0;
+        x = 0 - sw;
+    }*/
+    
+    if (i > 11)
+    {
+        i = 9;
     }
 }
